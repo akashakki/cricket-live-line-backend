dcc07619f4a3e2e0cec0c93dccd0ff2c495a072d
Akash Bais <mr.akash.bais@gmail.com>
2024-12-17 23:06:54 +0530

    add cron for IPL

--- src/services/ipl.service.js
+++ src/services/ipl.service.js
@@ -6,8 +6,9 @@ const getIPLOverview = async () => {
         // Fetch IPL overview data
         const overview = await IPLOverviewModel.findOne();
         const apiResponse = JSON.parse(overview.apiResponse);
-        // let allRounderPlayerIds = apiResponse.aTopAllRounderData.map(item => item._id);
-        // console.log("🚀 ~ file: ipl.service.js:10 ~ getIPLOverview ~ allRounderPlayerIds:", allRounderPlayerIds);
+        let allRounderPlayerIds = apiResponse.aTopAllRounderData.map(item => item._id);
+        let allBettersPlayerIds = apiResponse.aTopBatterData.map(item => item._id);
+        let allBowlPlayerIds = apiResponse.aTopBowlerData.map(item => item._id);
 
         // Fetch Top Picks: 6 players with highest SoldPrice and auctionStatus 'Sold'
         const topsPicks = await IPLAuctionPlayerModel.find({ auctionStatus: 'Sold', isCappedPlayer: true })
@@ -17,20 +18,20 @@ const getIPLOverview = async () => {
         // Fetch Top All-Rounders: 6 players with highest soldPrice, auctionStatus 'Sold', and playingRole 'all'
         const TopAllRounderData = await IPLAuctionPlayerModel.find({
             auctionStatus: { $in: ['Sold', 'Retained'] }, isCappedPlayer: true,
-            playingRole: 'all'
+            playingRole: 'all', apiPlayerId: { $in: allRounderPlayerIds }
         }).sort({ soldPrice: -1 })
             .limit(6);
 
         // Fetch Top Batters: 6 players with highest soldPrice, auctionStatus 'Sold', and playingRole 'bat'
         const TopBatterData = await IPLAuctionPlayerModel.find({
-            auctionStatus: { $in: ['Sold', 'Retained'] }, isCappedPlayer: true,
+            auctionStatus: { $in: ['Sold', 'Retained'] }, isCappedPlayer: true, apiPlayerId: { $in: allBettersPlayerIds },
             playingRole: 'bat'
         }).sort({ soldPrice: -1 })
             .limit(6);
 
         // Fetch Top Bowlers: 6 players with highest soldPrice, auctionStatus 'Sold', and playingRole 'bowl'
         const TopBowlerData = await IPLAuctionPlayerModel.find({
-            auctionStatus: { $in: ['Sold', 'Retained'] }, isCappedPlayer: true,
+            auctionStatus: { $in: ['Sold', 'Retained'] }, isCappedPlayer: true, apiPlayerId: { $in: allBowlPlayerIds },
             playingRole: 'bowl'
         }).sort({ soldPrice: -1 })
             .limit(6);
